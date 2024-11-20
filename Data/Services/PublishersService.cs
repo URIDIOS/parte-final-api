47ed58e5eacdb212fa87bd5a0c123981032f8e69
TheRev <cesarhumbertogarciafelix@gmail.com>
2024-11-20 14:11:04 -0700

    Final

---
+++ Data/Services/PublishersService.cs
@@ -0,0 +1,71 @@
+﻿using CREAR_API.Data.Excepcions;
+using CREAR_API.Data.Models;
+using CREAR_API.Data.ViewModels;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text.RegularExpressions;
+using Publisher = CREAR_API.Data.Models.Publisher;
+
+namespace CREAR_API.Data.Services
+{
+    public class PublishersService
+    {
+        private AppDbContext _context;
+        public PublishersService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //metodo para crear autor
+        public Publisher AddPublisher(PublisherVM publisher)
+        {
+            if (StringStartsWithNumber(publisher.Name)) throw new PublisherNameException("El nombre empieza con un numero",
+                publisher.Name);
+
+			var _publisher = new Models.Publisher()
+            {
+                Name = publisher.Name
+            };
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+
+            return _publisher;
+        }
+
+        public Publisher GetPublisherByID(int id) => _context.Publishers.FirstOrDefault(n => n.Id == id); 
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _publisherData = _context.Publishers.Where(n => n.Id == publisherId)
+                .Select(n => new PublisherWithBooksAndAuthorsVM()
+                {
+                    Name = n.Name,
+                    BookAuthors = n.Books.Select(n => new BookAuthorVM()
+                    {
+                        BookName = n.Titulo,
+                        BookAuthors = n.Book_Author.Select(n => n.Author.FullName).ToList()
+                    }).ToList()
+                }).FirstOrDefault();
+            return _publisherData;
+        }
+
+		internal void DeletePublisherById(int id)
+		{
+            var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == id);
+            if (_publisher != null)
+            {
+                _context.Publishers.Remove(_publisher);
+                _context.SaveChanges();
+            }
+            else 
+            {
+                throw new Exception($"La editora con el id: {id} no existe!");
+            }
+		}
+        private bool StringStartsWithNumber(string name) => (Regex.IsMatch(name, @"^\d"));
+
+
+	}
+}
